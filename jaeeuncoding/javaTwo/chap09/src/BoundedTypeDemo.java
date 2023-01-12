216073599658c860259fa75709844c800f495191
nowsilver <46918839+rosa2070@users.noreply.github.com>
2023-01-12 17:26:25 +0900

    Delete BoundedTypeDemo.java

--- jaeeuncoding/javaTwo/chap09/src/BoundedTypeDemo.java
+++
@@ -1,8 +0,0 @@
-public class BoundedTypeDemo {
-    public static void main(String[] args) {
-        Cup<Beer> c1 = new Cup<>();
-        Cup<Boricha> c2 = new Cup<>();
-//        Cup<String> c3 = new Cup<>();
-
-    }
-}
